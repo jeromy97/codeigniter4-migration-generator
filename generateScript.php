336f1662fe8e59dd5b3eb7e171369e34d77fec5f
Jeromy Hettinga <jeromy.hettinga97@casema.nl>
2020-05-04 21:48:06 +0200

    The migration generation script is now compatible with Codeigntiter 4

--- generateScript.php
+++ generateScript.php
@@ -5,11 +5,10 @@ $templateFile = "template.txt";
 file_exists($templateFile)
 	or exit ($templateFile . " not found...");
 
-echo "Enter migration name..." . PHP_EOL;
+echo "Enter the desired migration name in seperate words. For example: \"My beautifull Migration\"." . PHP_EOL;
 $migrationName = readline();
-$migrationName = str_replace(' ', '_', $migrationName);
 
-echo "Please enter the path of the Codeigniter 3 root folder or leave blank to generate the migration as a separate file..." . PHP_EOL;
+echo "Enter the path of the Codeigniter 4 root folder or leave blank to generate the migration as a separate file..." . PHP_EOL;
 $codeigniterDir = readline();
 
 if ($codeigniterDir == '') {
@@ -19,52 +18,24 @@ if ($codeigniterDir == '') {
 	}
 }
 else{
-	$path = $codeigniterDir . "\\application\\migrations\\";
+	$path = $codeigniterDir . "\\app\\Database\\Migrations\\";
 	if (!file_exists($path)) {
 		mkdir($path, 0777, false);
 	}
 }
 
-$migrationNumber = date('YmdHis');
+$migrationNumber = date('Y-m-d-His');
 
-$fileName = $path . $migrationNumber . '_' . strtolower($migrationName) . '.php';
+$migrationFileName = strtolower(str_replace(' ', '_', $migrationName));
+$fileName = $path . $migrationNumber . '_' . $migrationFileName . '.php';
 
+$migrationClassName = str_replace(' ', '', ucwords(strtolower($migrationName)));
 $content = file_get_contents($templateFile);
-$content = str_replace('{MIGRATION_NAME}', ucfirst(strtolower($migrationName)), $content);
+$content = str_replace('{MIGRATION_NAME}', $migrationClassName, $content);
 
 try {
 	file_put_contents($fileName, $content);
 	echo $fileName . " was succesfully created!" . PHP_EOL;
 } catch (Exception $e) {
 	exit ("Migration could not be created: " . $e->getMessage() . PHP_EOL);
-}
-
-if ($codeigniterDir != '') {
-	
-	do {
-		echo "Would you like the version_number in migration.php to be updated? (y/n)" . PHP_EOL;
-		$updateVersionNumber = strtolower(readline());
-	} while (!in_array($updateVersionNumber, array('y', 'n')));
-	
-	if ($updateVersionNumber == 'y') {
-		$configFile = $codeigniterDir . "\\application\\config\\migration.php";
-		if ( file_exists($configFile) ) {
-			$config = file_get_contents($configFile);
-			
-			$config = str_replace('$config[\'migration_enabled\'] = FALSE;', '$config[\'migration_enabled\'] = TRUE;', $config);
-			
-			$search = '$config[\'migration_version\'] = ';
-			$startPos = strpos($config, $search) + strlen($search);
-			$endPos = strpos($config, ';', $startPos);
-			
-			$config = substr_replace($config, $migrationNumber, $startPos, $endPos - $startPos);
-			
-			file_put_contents($configFile, $config);
-			
-			echo "migration.php updated succesfully!" . PHP_EOL;
-		}
-		else{
-			exit ($configFile . ' not found...');
-		}
-	}
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Codeigniter Migration Generator
 
-Create migration files for Codeigniter 3.
+Create migration files for Codeigniter 4.
 
 Just run the PHP script from the CLI or run codeigniterMigrationGenerator.bat.
